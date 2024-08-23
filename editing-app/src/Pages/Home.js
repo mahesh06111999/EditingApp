14328d2f5d109e822ea49cd4c4c78b5c7fa1dad6
Mahesh sr <48825634+mahesh06111999@users.noreply.github.com>
2024-08-23 18:53:17 +0530

    Add files via upload

---
+++ editing-app/src/Pages/Home.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  const [roomId, setRoomId] = useState('');
+  const [username, setUsername] = useState('');
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    console.log(id);
+    setRoomId(id);
+
+    toast.success('Created a new room');
+  };
+
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      toast.error('ROOM ID & username is required');
+      return;
+    }
+
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
+  };
+
+  const handleInputEnter = (e) => {
+    if (e.code === 'Enter') {
+      joinRoom();
+    }
+  };
+
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <h3 className="mainLabel">Create ROOM ID or Enter ROOM ID</h3>
+        <div className="inputGroup">
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
+            onKeyUp={handleInputEnter}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            onKeyUp={handleInputEnter}
+          />
+          <button onClick={joinRoom} className="btn joinBtn">
+            JOIN
+          </button>
+
+          <span className="createInfo">
+            If you don't have an invite then create &nbsp;
+            <a onClick={createNewRoom} href="/" className="createNewBtn">
+              NEW ROOM ID
+            </a>{' '}
+            here
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
